efa0862c30e33cbd81432d67013d8aa85673f820
michael alemayehu <mikealexiv565@gmail.com>
2021-02-08 19:16:15 +0300

    minor css adjustments to clock , spinner added until data is fetched (Question #1)

--- AsyncDemo/asset/js/loadPostsAPI.js
+++ AsyncDemo/asset/js/loadPostsAPI.js
@@ -1,10 +1,18 @@
 // UI Vars 
 const postDiv3 = document.getElementById('thePosts');
+let spinner = `
+<div class="ui segment">
+<p></p>
+<div class="ui active dimmer">
+  <div class="ui loader"></div>
+</div>
+</div>` 
 
 //Load Every thing ....
 document.addEventListener("DOMContentLoaded", () => {
     // load_fromPlaceHolder();
     loadDataNew();
+    startTime();
 });
 
 
@@ -49,35 +57,73 @@ function load_fromPlaceHolder() {
 
 }
 function loadDataNew() {
+   setTimeout(()=>{
     load_fromPlaceHolder_new().then(function(posts) {
-            //iterate over each post [100 posts]
-            let output = '';
-            posts.forEach(function(post) {
-                output += `
-        <div class="item">
-        <div class="image">
-            <img src=" https://images.unsplash.com/photo-1499482125586-91609c0b5fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80">
+        //iterate over each post [100 posts]
+        
+        let output = '';
+        posts.forEach(function(post) {
+            output += `
+    <div class="item">
+    <div class="image">
+        <img src=" https://images.unsplash.com/photo-1499482125586-91609c0b5fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80">
+    </div>
+    <div class="content">
+        <a class="header" href="#" id="bTitle">
+        ${post.title.toUpperCase()}
+        </a>
+        <div class="description">
+            <p id="bDesc">
+            ${post.body}
+            </p>
         </div>
-        <div class="content">
-            <a class="header" href="#" id="bTitle">
-            ${post.title.toUpperCase()}
-            </a>
-            <div class="description">
-                <p id="bDesc">
-                ${post.body}
-                </p>
-            </div>
-            <div class="extra">
-                <a class="ui floated basic violet button" href="#">Read Mores</a>
-            </div>
+        <div class="extra">
+            <a class="ui floated basic violet button" href="#">Read Mores</a>
         </div>
     </div>
+</div>
 `;
-            });
-            postDiv3.innerHTML = output;
-        })
-        .catch(function(err) {
-            console.log(err);
         });
+        postDiv3.innerHTML = output;
+        
+    }) .catch(function(err) {
+        console.log(err);
+    });
+   },5000, postDiv3.innerHTML = spinner)
+       
+
+}
+
+// UI Variables 
+const timerDemo = document.getElementById("timerDemo");
+//timer 
+function startTime() {
+   
+   
+            //retrieve date 
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        //get the AM / PM value 
+        let am_pm = h > 12 ? 'PM' : 'AM';
+        // Convert the hour to 12 format 
+        h = h % 12 || 12;
+        // add zero 
+        m = addZero(m);
+        s = addZero(s);
+        // Assign to the UI [p]
+        timerDemo.innerHTML =
+         `${h} : ${addZero(m)} : ${addZero(s)} ${am_pm }`;
+       //run the timer recursively 
+       setTimeout(startTime, 500);
+        }
+   
+    
+
+
+function addZero(i) {
+// add zero in front of numbers < 10
+if (i < 10) { i = "0" + i }    return i;
 
 }
